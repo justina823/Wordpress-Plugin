60103b688b69e931c2f471d4bc74c2c0a105f4b4
justina823 <laisumjustina.chan@my.jcu.edu.au>
2021-01-15 11:44:05 +1000

    update read.txt

---
+++ MyFristPlugin.php
@@ -0,0 +1,21 @@
+/**
+ Plugin Name:       My Frist Plugin
+ Plugin URI:        http://www.gocar.idv.tw/plugins/MyFristPlugin/
+ Description:       我的第一個Plugin
+ Version:           1.0
+ Author:            Henry Tsai
+ Author URI:        http://www.gocar.idv.tw/
+
+ {Plugin Name} is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 2 of the License, or
+ any later version.
+  
+ {Plugin Name} is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+   
+ You should have received a copy of the GNU General Public License
+ along with {Plugin Name}. If not, see {License URI}
+*/
